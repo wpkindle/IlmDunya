241e03105a7b3766da4f44ab2de96b8fef3e6f5b
Abdul Khaliq <abdulkhaliqwebdeveloper@gmail.com>
2026-09-07 08:34:21 +0500

    chore(copy): update Cambridge & Matric to Playgroup to FSc

--- src/components/AnimatedHeroBackground.jsx
+++ src/components/AnimatedHeroBackground.jsx
@@ -226,7 +226,7 @@ export default function AnimatedHeroBackground() {
           <GraduationCap className="w-4 h-4" />
         </div>
         <div className="text-left">
-          <span className="text-[11px] font-bold block text-[#0c311e]">Cambridge &amp; Matric</span>
+          <span className="text-[11px] font-bold block text-[#0c311e]">Playgroup to FSc</span>
           <span className="text-[9px] text-[#b85d34] block font-mono">Pre-Medical &amp; Pre-Eng</span>
         </div>
       </div>
